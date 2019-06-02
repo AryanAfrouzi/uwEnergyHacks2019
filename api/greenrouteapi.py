3dc9aa90dae45a04880347def1b55ce788bb2fb5
Cristian Bicheru <xxthegoldenbiscuitxx@gmail.com>
2019-06-02 08:58:11 -0400

    thanos merge thanos merge

---
+++ api/greenrouteapi.py
@@ -0,0 +1,94 @@
+import vincenty
+import math
+import requests
+import json
+import polyline
+
+API_KEY = "AIzaSyBAc8hF0Rab50oJJRbemflUPbB-7lCXTmk"
+
+emissionsData = {'Midsize Sedan CV':[(-5, 2.21), (0, 3.21), (5, 6.11)], 'Midsize Sedan HEV':[(-5, 0.06), (0, 1.98), (5, 4.9)], 'SUV CV':[(-5, 3.07), (0, 4.32), (5, 8.32)], 'SUV HEV':[(-5, 0.15), (0, 3), (5, 7.03)]}
+
+def applyAlgo(emissionsData):
+    algodData = {}
+    for car in list(emissionsData.keys()):
+        p1, p2, p3 = [x[1] for x in emissionsData[car]]
+        d = (p1*p3-p2**2)/(p3-2*p2+p1)
+        c = p2-d
+        a = ((p3-d)/(p2-d))**0.2
+        algodData[car] = [d, c, a]
+    return algodData
+
+algodData = applyAlgo(emissionsData)
+
+def fuelFactor(vel):
+    return 5.341880342*10**(-9)*vel**4 - 6.891025641*10**(-7)*vel**3 - 2.583867521*10**(-4)*vel**2 + 3.734615385*10**(-2)*vel + 0.2
+
+def carbonFunction(distance, slope, data, vel):
+    grade = 100*slope
+    galpermile = (data[1]*data[2]**grade+data[0])/(100*fuelFactor(vel))
+    return [galpermile*(distance*0.621371)*8887, 1/galpermile]
+
+def distcarb(points, elevations, data, steps):
+    # carbonFunction = f(grade)=km/L
+    lenTotal = 0
+    carbon = 0
+    avgmpg = 0
+    cVel = 0
+    distToNext = -0.0001
+    step = 0
+    # vincenty's formula with height approximation using exponential moving averages to smooth data
+    lastEMA = sum([(elevations[i]-elevations[i+1])/(math.sqrt(vincenty.vincenty(points[i], points[i+1])**2+((elevations[i]-elevations[i+1])/1000)**2)*1000) for i in range(0, 3)])/3
+    for i in range(0, len(points)-1):
+        if lenTotal > distToNext:
+            while lenTotal > distToNext and step < len(steps)-1:
+                step += 1
+                distToNext += steps[step]['distance']['value']/1000
+            cVel = steps[step]['distance']['value']/(steps[step]['duration']['value'])*3.6
+        dist = math.sqrt(vincenty.vincenty(points[i], points[i+1])**2+((elevations[i]-elevations[i+1])/1000)**2)
+        lastEMA = (elevations[i+1]-elevations[i])/(dist*1000)*(0.125)+lastEMA*(0.875)
+        dcarbon, mpg = carbonFunction(dist, lastEMA, data, cVel)
+        carbon += dcarbon
+        avgmpg += mpg*dist
+        lenTotal += dist
+    return [lenTotal, carbon, avgmpg/lenTotal]
+
+def greenroutealgo(location1, location2, carType):
+    directionsAPIData = json.loads(requests.get("https://maps.googleapis.com/maps/api/directions/json?origin="+location1.replace(' ', '+')+"&destination="+location2.replace(' ', '+')+"&alternatives=true&key="+API_KEY).text)
+    routes = directionsAPIData['routes']
+    statistics = []
+    for route in routes:
+        points = route['overview_polyline']['points']
+        if int(route['legs'][0]['distance']['value']/100) > 512:
+            samples = 512
+        else:
+            samples = int(route['legs'][0]['distance']['value']/100)
+        elevationsJSON = json.loads(requests.get("https://maps.googleapis.com/maps/api/elevation/json?path=enc:"+points+"&samples="+str(samples)+"&key="+API_KEY).text)
+        elevations = [x['elevation'] for x in elevationsJSON['results']]
+        carData = algodData[carType]
+        pointsd = [(x['location']['lat'], x['location']['lng']) for x in elevationsJSON['results']]
+        statistics.append(distcarb(pointsd, elevations, carData, route['legs'][0]['steps']))
+
+    minCarbon = 9999999999999999999
+    minRoute = -1
+    maxCarbon = 0
+    maxcs = 0
+    for each in statistics:
+        minRoute += 1
+        if each[1] < minCarbon:
+            minCarbon = each[1]
+            avgmpg = each[2]
+            break
+    for each in statistics:
+        if each[1] > maxCarbon:
+            maxCarbon = each[1]
+    return {"startAddress":routes[minRoute]['legs'][0]['start_address'],\
+                      "endAddress":routes[minRoute]['legs'][0]['end_address'],\
+                      "distance":routes[minRoute]['legs'][0]['distance']['text'],\
+                      "distancen":routes[minRoute]['legs'][0]['distance']['value'],\
+                      "time":routes[minRoute]['legs'][0]['duration']['text'],\
+                      "timen":routes[minRoute]['legs'][0]['duration']['value'],\
+                      "carbon":minCarbon,\
+                      "averagempg":avgmpg,\
+                      "maxcarbonsaved":maxCarbon-minCarbon,\
+                      "directions":[x['html_instructions'] for x in routes[minRoute]['legs'][0]['steps']],\
+                      "path":polyline.decode(routes[minRoute]['overview_polyline']['points'])}
---
+++ frontend/index.js
@@ -0,0 +1,244 @@
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    //get id token
+    user.getIdToken().then(function(idToken) {
+      userIdToken = idToken;
+    });
+
+    // User is signed in.
+
+    document.getElementById("user_div").style.display = "block";
+    document.getElementById("login_div").style.display = "none";
+
+    var user = firebase.auth().currentUser;
+
+    if(user != null){
+
+      var email_id = user.email;
+      document.getElementById("user_para").innerHTML = "Welcome User : " + email_id;
+
+    }
+
+  } else {
+    // No user is signed in.
+
+    document.getElementById("user_div").style.display = "none";
+    document.getElementById("login_div").style.display = "block";
+
+  }
+});
+
+function login(){
+  var userEmail = document.getElementById("email_field").value;
+  var userPass = document.getElementById("password_field").value;
+
+  firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+
+    window.alert("Error : " + errorMessage);
+
+    // ...
+  });
+
+}
+
+function logout(){
+  firebase.auth().signOut();
+}
+
+$(document).on('click', ".car-type", function () {
+  $('.selected-car').removeClass('active');
+  $('.selected-car').removeClass('selected-car');
+  $(this).addClass('active');
+  $(this).addClass('selected-car');
+});
+
+var backendhosturl = 'https://green-route-api.appspot.com/api'
+//create map using javascript api
+function initMap() {
+  //if data is submitted
+  document.getElementById("submit").addEventListener("click", function(){
+    var start = $('.location_start').val();
+    var destination = $('.location_destination').val();
+    var carModel = $('.selected-car').html();
+
+    if(carModel == 'None') {
+      alert("No Car Selected");
+    }
+    else {
+      $.ajax(backendhosturl, {
+        headers: {
+          'Authorization': 'Bearer ' + userIdToken,
+          'Access-Control-Allow-Origin': '*'
+        },
+        method: 'POST',
+        data: JSON.stringify({'location1': start, 'location2': destination, 'cartype': carModel}),
+        contentType : 'application/json'
+      }).then(function(recieved) {
+        data = recieved['path'];
+
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 7,
+          center: {lat:data[Math.floor((data.length)/2)][0], lng: data[Math.floor((data.length)/2)][1]}
+        })
+      
+        var startMarker = new google.maps.Marker({
+          position: {lat:data[0][0], lng: data[0][1]},
+          map: map,
+          title: 'Start',
+          label: 'S',
+          animation: google.maps.Animation.DROP
+        });
+
+        var endMarker = new google.maps.Marker({
+          position: {lat:data[data.length-1][0], lng: data[data.length-1][1]},
+          map: map,
+          title: 'End',
+          label: 'E',
+          animation: google.maps.Animation.DROP
+        });
+      
+        var poly = new google.maps.Polyline({
+          map: map,
+          path: [],
+          geodesic: true,
+          strokeColor: '#00FF00',
+          strokeOpacity: 1.0,
+          strokeWeight: 2
+        })
+
+        var route = [];
+        for(i=0; i < data.length; i++) {
+          route.push({lat: data[i][0], lng: data[i][1]});
+        }
+        var path = poly.getPath();
+        path = [];
+        path = route;
+        // update the polyline with the updated path
+        poly.setPath(path); 
+        //scrolls down to map
+        document.getElementById('map').style.height = "500px";
+
+        $('html, body').animate({
+          scrollTop: $("#map").offset().top
+        }, 1000);
+
+        retrieveStats();
+      }); 
+    } 
+  });
+} 
+
+var stats;
+
+function retrieveStats() {
+  $.ajax(backendhosturl, {
+    headers: {
+      'Authorization': 'Bearer ' + userIdToken,
+    },
+  }).then(function(data) {
+    stats = data;
+    console.log(stats);
+  });
+}
+//populate stats for cars
+document.getElementById("car1").addEventListener("click", function(){
+  openTopNav();
+  $('.activeCar').removeClass('activeCar');
+  $('#car1').addClass('activeCar');
+
+  $('.total-carbon').html((stats[0][0]/1000).toFixed(2).toString() + 'kg');
+  $('.average-carbon').html((stats[0][1]/1000).toFixed(2).toString() + 'kg');
+  $('.total-carbon-saved').html((stats[0][3]/1000).toFixed(2).toString() + 'kg');
+  $('.average-carbon-saved').html((stats[0][4]/1000).toFixed(2).toString() + 'kg');
+  $('.average-mpg').html(stats[0][6].toFixed(2).toString() + 'MPG');
+  $('.total-miles-driven').html((stats[0][8]/1000).toFixed(2).toString() + 'km');
+  $('.average-miles-driven').html((stats[0][9]/1000).toFixed(2).toString() + 'km');
+  $('.average-speed').html((stats[0][11]/1000).toFixed(2).toString() + 'km');
+});
+
+document.getElementById("car2").addEventListener("click", function(){
+  openTopNav();
+  $('.activeCar').removeClass('activeCar');
+  $('#car2').addClass('activeCar');
+
+  $('.total-carbon').html((stats[1][0]/1000).toFixed(2).toString() + 'kg');
+  $('.average-carbon').html((stats[1][1]/1000).toFixed(2).toString() + 'kg');
+  $('.total-carbon-saved').html((stats[1][3]/1000).toFixed(2).toString() + 'kg');
+  $('.average-carbon-saved').html((stats[1][4]/1000).toFixed(2).toString() + 'kg');
+  $('.average-mpg').html(stats[1][6].toFixed(2).toString() + 'MPG');
+  $('.total-miles-driven').html((stats[1][8]/1000).toFixed(2).toString() + 'km');
+  $('.average-miles-driven').html((stats[1][9]/1000).toFixed(2).toString() + 'km');
+  $('.average-speed').html((stats[1][11]/1000).toFixed(2).toString() + 'km');
+});
+
+document.getElementById("car3").addEventListener("click", function(){
+  openTopNav();
+  $('.activeCar').removeClass('activeCar');
+  $('#car3').addClass('activeCar');
+
+  $('.total-carbon').html((stats[2][0]/1000).toFixed(2).toString() + 'kg');
+  $('.average-carbon').html((stats[2][1]/1000).toFixed(2).toString() + 'kg');
+  $('.total-carbon-saved').html((stats[2][3]/1000).toFixed(2).toString() + 'kg');
+  $('.average-carbon-saved').html((stats[2][4]/1000).toFixed(2).toString() + 'kg');
+  $('.average-mpg').html(stats[2][6].toFixed(2).toString() + 'MPG');
+  $('.total-miles-driven').html((stats[2][8]/1000).toFixed(2).toString() + 'km');
+  $('.average-miles-driven').html((stats[2][9]/1000).toFixed(2).toString() + 'km');
+  $('.average-speed').html((stats[2][11]/1000).toFixed(2).toString() + 'km');
+});
+
+document.getElementById("car4").addEventListener("click", function(){
+  openTopNav();
+  $('.activeCar').removeClass('activeCar');
+  $('#car4').addClass('activeCar');
+
+  $('.total-carbon').html((stats[3][0]/1000).toFixed(2).toString() + 'kg');
+  $('.average-carbon').html((stats[3][1]/1000).toFixed(2).toString() + 'kg');
+  $('.total-carbon-saved').html((stats[3][3]/1000).toFixed(2).toString() + 'kg');
+  $('.average-carbon-saved').html((stats[3][4]/1000).toFixed(2).toString() + 'kg');
+  $('.average-mpg').html(stats[3][6].toFixed(2).toString() + 'MPG');
+  $('.total-miles-driven').html((stats[3][8]/1000).toFixed(2).toString() + 'km');
+  $('.average-miles-driven').html((stats[3][9]/1000).toFixed(2).toString() + 'km');
+  $('.average-speed').html((stats[3][11]/1000).toFixed(2).toString() + 'km');
+});
+
+
+//Maps autocomplete api
+function initializeAutocomplete() {
+  var startInput = document.getElementById('start');
+  startAuto = new google.maps.places.Autocomplete(startInput);
+  var endInput = document.getElementById('end');
+  endAuto = new google.maps.places.Autocomplete(endInput);
+}
+
+initMap();
+initializeAutocomplete();
+
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "275px";
+  document.getElementById("main").style.marginLeft = "275px";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+} 
+
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openTopNav() {
+  document.getElementById("topsidenav").style.height = "500px";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeTopNav() {
+  document.getElementById("topsidenav").style.height = "0";
+} 
+
+
+
+
+
